7444ae64bc31961798da345e25e4d39bbf10883c
arosspope <andrew.pope456@gmail.com>
2020-01-09 11:33:08 +1100

    roulette challenge

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
 
 Learning embedded development in rust with the [discovery book](https://rust-embedded.github.io/discovery).
 
+## Setup
+
+1. Install packages (Ubuntu 18.04)
+```
+$ apt-get install gdb-multiarch minicom openocd
+```
+
+2. udev rules
+```
+$ cat /etc/udev/rules.d/99-ftdi.rules
+...
+# FT232 - USB <-> Serial Converter
+ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", MODE:="0666"
+...
+$ cat /etc/udev/rules.d/99-openocd.rules
+...
+# STM32F3DISCOVERY rev A/B - ST-LINK/V2
+ATTRS{idVendor}=="0483", ATTRS{idProduct}=="3748", MODE:="0666"
+
+# STM32F3DISCOVERY rev C+ - ST-LINK/V2-1
+ATTRS{idVendor}=="0483", ATTRS{idProduct}=="374b", MODE:="0666"
+...
+$ sudo udevadm control --reload-rules
+```
 
 ## Running
 
@@ -67,6 +91,7 @@ Breakpoint 1 at 0x800018c: file src/05-led-roulette/src/main.rs, line 10.
 ...
 /tmp$ itmdump -F -f itm.txt
 ```
+
 2. Instruct OpenOCD to redirect the ITM output into the same file that itmdump is watching:
 ```
 (gdb) # globally enable the ITM and redirect all output to itm.txt
---
+++ src/challenges/roulette.rs
@@ -0,0 +1,58 @@
+#![deny(unsafe_code)]
+#![allow(unused_imports)]
+#![no_main]
+#![no_std]
+
+// The following imports are important for compilation
+use panic_halt;
+use f3::hal::prelude::*;
+use cortex_m_rt::entry;
+
+use f3::led::Leds;
+use f3::hal::{delay::Delay, stm32f30x};
+
+fn init() -> (Delay, Leds) {
+    let cp = cortex_m::Peripherals::take().unwrap();    // Obtain exclusive access to all the cortex-m peripherals ONCE
+    let dp = stm32f30x::Peripherals::take().unwrap();   // Obtain exclusive access to all the stm32f30x specific peripherals
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();                   // RCC - Reset and Clock Control
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr); // Accessing the RCC_CFGR register and freezing the clock configuration
+    let delay = Delay::new(cp.SYST, clocks);
+    let leds = Leds::new(dp.GPIOE.split(&mut rcc.ahb));
+
+    (delay, leds)
+}
+
+#[entry]
+fn main() -> ! {
+    let (mut delay, mut leds): (Delay, Leds) = init();
+
+    const period_ms: u8             = 25;
+
+    let total_headings              = leds.len();
+
+    let mut heading                 = 0;
+    let mut active_heading_count    = 2;
+
+    leds[heading].on();
+    leds[(heading + 1) % total_headings].on();
+
+    loop {
+        delay.delay_ms(period_ms);
+
+        match active_heading_count {
+            0 => {
+                leds[heading].off();
+                heading = (heading + 1) % total_headings;
+                active_heading_count = 2;
+            },
+            1 => {
+                leds[(heading + 1) % total_headings].on();
+                active_heading_count -= 1;
+            },
+            _ => active_heading_count -= 1,
+        }
+    }
+}
